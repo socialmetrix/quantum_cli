ba5fd732e3f7ba0a4c33616a2c60b76681ea2e40
Gustavo Arjones <garjones@socialmetrix.com>
2015-10-31 01:22:29 -0300

    Added arg parser. Command ADD created

--- quantum_cli.py
+++ quantum_cli.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import requests, json
 import time
 
@@ -78,36 +76,104 @@ class QuantumApi():
 #
 # Main
 
+# from sys import argv
+# import os, sys
+# import argparse
+#
+# def main(argv):
+#   _, filename = argv
+#
+#   secret = os.environ.get('QUANTUM_SECRET')
+#   if (secret == None):
+#     print("Secret must be defined. Please set a environment variable QUANTUM_SECRET with your app secret")
+#     sys.exit(1)
+#
+#   api = QuantumApi()
+#
+#   print("Authenticating ...")
+#   authData = api.authenticate(secret)
+#
+#   print("Creating project ...")
+#   project_id = api.create_project()
+#
+#   with open(filename, 'r') as f:
+#     for line in f:
+#       profile = line.rstrip('\r\n')
+#       print("\tAdding profile {0} to project {1}".format(profile, project_id))
+#       api.add_profile(profile)
+#   f.closed
+#
+#   print("\n\nCheck the Project URL here: " + api.project_home_url())
+#   print("\n===== Done =====")
+#   pass
+
+def check_secret(secret):
+  if(secret == None):
+    secret = os.environ.get('QUANTUM_SECRET')
+    if (secret == None):
+      print("Secret must be defined. Please set a environment variable QUANTUM_SECRET or use the parameter --secret with your app secret")
+      sys.exit(1)
+  return secret
+
+
+def add_profiles(file, project_id, secret):
+  api = QuantumApi()
+  
+  print("Authenticating ...")
+  api.authenticate(check_secret(secret))
+  
+  if(project_id == None):
+    print("Creating project ...")
+    project_id = api.create_project()
+  else:
+    print("Using passed project_id: {}".format(project_id))
+    api.project_id = project_id
+
+  for line in file:
+    profile = line.rstrip('\r\n')
+    print("\tAdding profile {0} to project {1}".format(profile, project_id))
+    api.add_profile(profile)
+  
+  print("\n\nCheck the Project URL here: " + api.project_home_url())
+  pass
+
+
+def view_profiles(project_id, secret):
+  print("App secret is {0}".format(check_secret(secret)))
+  print("view_profiles from project: " + project_id)
+  pass
+
+
 from sys import argv
 import os, sys
 import argparse
 
 def main(argv):
-  _, filename = argv
+  parser = argparse.ArgumentParser(description="Quantum Client Utility")
+  parser.add_argument("--secret", help="set the secret id")
 
-  secret = os.environ.get('QUANTUM_SECRET')
-  if (secret == None):
-    print("Secret must be defined. Please set a environment variable QUANTUM_SECRET with your app secret")
+  subparsers = parser.add_subparsers(help='commands', dest='command')
+
+  # A list command
+  add_parser = subparsers.add_parser('add', help='Add profiles to a project')
+  add_parser.add_argument("file", help="path for the file containing profiles to add", type=file)
+  add_parser.add_argument("--project", help="the project id to insert", type=int)
+
+  # A create command
+  view_parser = subparsers.add_parser('view', help='View project profiles')
+  view_parser.add_argument("project", help="project id")
+
+  try:
+      args = parser.parse_args()
+      if (args.command == 'add'):
+        add_profiles(args.file, args.project, args.secret)
+        
+      elif (args.command == 'view'):
+        view_profiles(args.project, args.secret)
+      
+  except IOError, msg:
+    parser.error(str(msg))
     sys.exit(1)
-
-  api = QuantumApi()
-  print("Authenticating ...")
-  authData = api.authenticate(secret)
-
-  print("Creating project ...")
-  project_id = api.create_project()
-  print("Project created, id: {0}".format(project_id))
-
-  with open(filename, 'r') as f:
-    for line in f:
-      profile = line.rstrip('\r\n')
-      print("Adding profile {0} to project {1}".format(profile, project_id))
-      api.add_profile(profile)
-  f.closed
-
-  print("\n\nCheck the Project URL here: " + api.project_home_url())
-  print("\n===== Done =====")
-  pass
 
 if __name__ == "__main__":
   main(sys.argv)
